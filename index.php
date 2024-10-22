a7b0c895cd36a3df38d36c9cae76cbce10d9a4d6
Svenlaa <mail@svenlaa.com>
2024-10-22 13:32:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>Quickdex</title>
 <link rel="stylesheet" href="https://svenlaa.com/style.css" />
+<style>
+    #searchfield {
+        font-size: 0.6em;
+        border-width: 0px;
+        font-family: inherit;
+        outline: 0;
+        border-radius: 0.5em;
+    }
+</style>
 
-<h1>Quickdex</h1>
+<h1 style="display: inline">Quickdex <input id="searchfield" type="text" autofocus /></h1>
 
 <?php
     $filenames = scandir('.');
@@ -12,17 +21,28 @@
     });
 ?>
 
-<!-- <label style="margin: 1em 0">
-    Go to
-    <input type="text" id="input" autofocus>
-</label> -->
-
 <ul></ul>
 
 <script>const directories = <?= json_encode(array_values($directories)) ?> ?? [];</script>
 <script>
+    const searchField =document.querySelector('#searchfield')
     const listHtml = directories.map(directory => {
-        return `<li><a href="/${directory}">${directory}</a></li>`
+        return `<li><a data-directory="${directory}" href="/${directory}">${directory}</a></li>`
     }).join('');
-    document.querySelector('ul').innerHTML = listHtml ? listHtml : 'No directories found :('  
+    document.querySelector('ul').innerHTML = listHtml ? listHtml : 'No directories found :(';
+    document.body.addEventListener('click', e => {searchField.focus();});
+    document.body.addEventListener('keydown', e => {
+        const dir = e.target.dataset.directory
+        if (e.key === 'Enter' && dir !== undefined) {
+            e.preventDefault();
+            window.location = e.ctrlKey ? `https://${dir}.test` : `https://localhost/${dir}`;
+            return;
+        }
+    });
+    searchField.addEventListener('input', e => {
+        const searchValue = searchField.value;
+        const filteredDirectories = directories.filter((directory) => directory.toLowerCase().includes(searchValue.toLowerCase()));
+        const listhtml2 = filteredDirectories.map(directory => `<li><a data-directory="${directory}" href="/${directory}">${directory}</a></li>`).join('');
+        document.querySelector('ul').innerHTML = listhtml2  ? listhtml2  : 'No directories found :(';
+    })
 </script>
